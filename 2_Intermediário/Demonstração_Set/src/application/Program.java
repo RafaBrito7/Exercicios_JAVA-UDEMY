e4da8688f52c96b3c7ceb4055901b727f9fa99be
Rafael Brito <rafabrito.office@gmail.com>
2020-05-25 10:13:35 -0300

    Add Exemplo Trabalhando com Set

---
+++ 2_Intermediário/Demonstração_Set/src/application/Program.java
@@ -0,0 +1,35 @@
+package application;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Set<Integer> a = new TreeSet<>(Arrays.asList(0,2,4,5,6,8,10)); 
+		Set<Integer> b = new TreeSet<>(Arrays.asList(5,6,7,8,9,10));
+		
+		/*
+		 * HashSet = Mais rápido e não ordenado
+		 * linkedHashSet = Velocidade intermediária e os elementos continuam na ordem que foram adicionados
+		 * TreeSet = Mais lento e ordenado pelo CompareTo automaticamente
+		 */
+		
+		//union = Une os elementos de ambos conjuntos sem repetição
+		Set<Integer> c = new TreeSet<>(a); // Instanciando um set c com uma cópia do Set a
+		c.addAll(b);
+		System.out.println(c);
+		
+		//intersection = Os elementos em comum entre ambos os conjuntos
+		Set<Integer> d = new TreeSet<>(a);
+		d.retainAll(b);
+		System.out.println(d);
+		//difference
+		Set<Integer> e = new TreeSet<>(a);
+		e.removeAll(b);
+		System.out.println(e);
+	}
+
+}
