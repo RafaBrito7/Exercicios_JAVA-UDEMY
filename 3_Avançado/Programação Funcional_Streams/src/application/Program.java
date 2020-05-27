5c297799f2cd6454b700690ea6081c2e4a7e691f
Rafael Brito <rafabrito.office@gmail.com>
2020-05-27 14:18:22 -0300

    Exemplo de Stream - Pipeline

--- 3_Avançado/Programação Funcional_Streams/src/application/Program.java
+++ 3_Avançado/Programação Funcional_Streams/src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Program {
@@ -23,6 +24,25 @@ public class Program {
 		Stream<Long> str2 = 
 				Stream.iterate(new Long[] {0L, 1L}, x -> new Long[] {x[1], x[0] + x[1]}).map(x -> x[0]);
 		System.out.println(Arrays.toString(str2.limit(10).toArray()));
+		
+		System.out.println("--------------------PIPELINE--------------------------");
+		//Pipeline = operações em streams retornam novas streams. Então é possível
+		//			 criar uma cadeia de operações (fluxo de processamento)
+		
+		Stream<Integer> str3 = list.stream().map(x -> x * 2);
+		System.out.println(Arrays.toString(str3.toArray()));
+		
+		int sum = list.stream().reduce(0, (x,y) -> x + y);
+		System.out.println("Sum = " + sum);
+		
+		List<Integer> list2 = list.stream()
+				.filter(x -> x % 2 == 0)
+				.map(x -> x * 10)
+				.collect(Collectors.toList());
+		
+		System.out.println(Arrays.toString(list2.toArray()));
+		
 	}
+	
 
 }
