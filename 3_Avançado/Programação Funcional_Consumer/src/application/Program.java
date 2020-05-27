2eecd5c9a0fcfee78c848c666107f1e745bf5734
Rafael Brito <rafabrito.office@gmail.com>
2020-05-27 09:26:43 -0300

    Consumer - Implementando Interface

---
+++ 3_Avançado/Programação Funcional_Consumer/src/application/Program.java
@@ -0,0 +1,26 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.Product;
+import util.MyConsumer;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		List<Product> list = new ArrayList<>();
+
+		list.add(new Product("Teclado", 90.00));
+		list.add(new Product("Notebook", 1200.00));
+		list.add(new Product("Tablet", 45.00));
+		list.add(new Product("Mouse", 450.00));
+
+		list.forEach(new MyConsumer());
+		
+		list.forEach(System.out::println);
+
+	}
+
+}
