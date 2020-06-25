917868e334da1cb36710bb76c9487ffc906299e5
Rafael Brito <rafabrito.office@gmail.com>
2020-06-25 17:56:34 -0300

    JPA/Hibernate - Introdução Básica

---
+++ 3_Avançado/JPA e Hibernate exemplo Básico/aulajpamaven/src/main/java/aplication/Program.java
@@ -0,0 +1,30 @@
+package aplication;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import dominio.Pessoa;
+
+
+public class Program {
+
+	public static void main(String[] args) {
+		// Sempre que houver uma ação que não seja uma simples consulta ao DB, é necessário uma transaction
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		Pessoa p = em.find(Pessoa.class, 2);
+		em.getTransaction().begin();
+		em.remove(p);
+		em.getTransaction().commit();
+		
+		System.out.println(p);
+		
+		System.out.println("Pronto!");
+		em.close();
+		emf.close();
+	}
+
+}
